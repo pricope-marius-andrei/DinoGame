4f3ff20c0eb37cfe7950d0d0ae234c70936b0e7b
MariusPricope <mariuspricope66@gmai.com>
2022-08-26 18:24:28 +0300

    remove enemy when he is outside of frame

--- actors.js
+++ actors.js
@@ -1,1 +1,14 @@
 export const player = document.getElementById("player")
+export const enemies = document.getElementsByClassName("enemy")
+
+export function EnemyOutside()
+{
+    for(let i = 0 ; i < enemies.length; i++)
+    {
+        const position = enemies[0].getBoundingClientRect();
+        if(position.x < 0)
+        {
+            enemies[0].remove();
+        }
+    }
+}
